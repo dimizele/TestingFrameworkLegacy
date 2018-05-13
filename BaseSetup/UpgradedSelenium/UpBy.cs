95d95928791423763a10373b380bbf5dd2cfc343
dimizele <dimitri_zelkov@hotmail.com>
2018-05-13 14:39:28 +0200

    Added an overload class for Selenium By locator and WebElement

---
+++ BaseSetup/UpgradedSelenium/UpBy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace BaseSetup.UpgradedSelenium
+{
+    public class UpBy : By
+    {
+        public static By AttributeValue(string tagName, string attributeName, string attributeValue)
+        {
+            return CssSelector($"{tagName}[{attributeName}='{attributeValue}']");
+        }
+    }
+}
---
+++ BaseSetup/UpgradedSelenium/UpWebElement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+
+namespace BaseSetup.UpgradedSelenium
+{
+    public class UpWebElement : IWebElement, IWrapsElement
+    {
+        public IWebElement FindElement(By @by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By @by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendKeys(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TagName { get; }
+        public string Text { get; }
+        public bool Enabled { get; }
+        public bool Selected { get; }
+        public Point Location { get; }
+        public Size Size { get; }
+        public bool Displayed { get; }
+        public IWebElement WrappedElement { get; }
+    }
+}
